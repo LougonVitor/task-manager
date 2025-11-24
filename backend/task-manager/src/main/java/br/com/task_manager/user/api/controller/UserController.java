8424b046d7a0ab02db118bbaa6fe9c390eb26d1e
Vítor Lougon <vitor.lougon@gmail.com>
2025-11-24 10:55:11 -0300

    refactor: adjusting DDD relationship betwenn API layer and application

--- backend/task-manager/src/main/java/br/com/task_manager/user/api/controller/UserController.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/api/controller/UserController.java
@@ -2,9 +2,11 @@ package br.com.task_manager.user.api.controller;
 
 import br.com.task_manager.user.api.dto.UserRequestDto;
 import br.com.task_manager.user.api.dto.UserResponseDto;
+import br.com.task_manager.user.api.mapper.UserMapper;
+import br.com.task_manager.user.application.dto.CreateUserCommand;
+import br.com.task_manager.user.application.dto.UserServiceResponseDto;
 import br.com.task_manager.user.application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +21,12 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser (@RequestBody UserRequestDto request) {
-        UserResponseDto response = this.userService.createUser(request);
+        CreateUserCommand userCommand = UserMapper.toCreateUserCommand(request);
+
+        UserServiceResponseDto serviceResponse = this.userService.createUser(userCommand);
+
+        UserResponseDto response = UserMapper.toResponseDto(serviceResponse);
+
         return ResponseEntity.ok(response);
     }
 }
--- backend/task-manager/src/main/java/br/com/task_manager/user/application/service/UserService.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/application/service/UserService.java
@@ -1,7 +1,7 @@
 package br.com.task_manager.user.application.service;
 
-import br.com.task_manager.user.api.dto.UserRequestDto;
-import br.com.task_manager.user.api.dto.UserResponseDto;
+import br.com.task_manager.user.application.dto.CreateUserCommand;
+import br.com.task_manager.user.application.dto.UserServiceResponseDto;
 import br.com.task_manager.user.domain.entity.UserEntity;
 import br.com.task_manager.user.domain.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +12,11 @@ public class UserService {
     @Autowired
     private IUserRepository userRepository;
 
-    public UserResponseDto createUser (UserRequestDto requestDto) {
+    public UserServiceResponseDto createUser (CreateUserCommand requestDto) {
         UserEntity userEntity = new UserEntity(requestDto.username(),requestDto.email(), requestDto.password());
 
         UserEntity createdUser = this.userRepository.createUser(userEntity);
 
-        return new UserResponseDto(createdUser.getId(), createdUser.getUsername(), createdUser.getEmail());
+        return new UserServiceResponseDto(createdUser.getId(), createdUser.getUsername(), createdUser.getEmail());
     }
 }
