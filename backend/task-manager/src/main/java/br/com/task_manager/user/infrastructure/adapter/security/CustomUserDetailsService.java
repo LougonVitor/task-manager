40088d38f076ba5cc9b8cab923d478ef032f08ea
Vítor Lougon <vitor.lougon@gmail.com>
2025-12-03 18:08:32 -0300

    feat: starting jwt classes

---
+++ backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/adapter/security/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package br.com.task_manager.user.infrastructure.adapter.security;
+
+import br.com.task_manager.user.domain.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
---
+++ backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/adapter/security/UserSecurityDetails.java
@@ -0,0 +1,37 @@
+package br.com.task_manager.user.infrastructure.adapter.security;
+
+import br.com.task_manager.user.domain.entity.UserEntity;
+import br.com.task_manager.user.domain.valueobject.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserSecurityDetails implements UserDetails {
+    private final UserEntity user;
+
+    public UserSecurityDetails(UserEntity user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.user.getRole() == UserRole.ADMIN ) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+}
