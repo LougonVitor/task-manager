cc62ecd1e1dc62ceab228a52cbde2bb498e5e05c
Vítor Lougon <vitor.lougon@gmail.com>
2026-01-01 21:18:52 -0300

    refactor: simplifying the build of user details

--- backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/security/CustomUserDetailsService.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/security/CustomUserDetailsService.java
@@ -3,10 +3,13 @@ package br.com.task_manager.user.infrastructure.security;
 import br.com.task_manager.user.domain.entity.UserEntity;
 import br.com.task_manager.user.domain.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
@@ -16,10 +19,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity userEntity = this.userRepository.findByUsername(username);
 
-        return org.springframework.security.core.userdetails.User
-                .withUsername(userEntity.getUsername())
-                .password(userEntity.getPassword())
-                .roles(userEntity.getRole().toString())
-                .build();
+        return User
+            .withUsername(userEntity.getUsername())
+            .password(userEntity.getPassword())
+            .roles(userEntity.getRole().toString())
+            .build();
     }
 }
