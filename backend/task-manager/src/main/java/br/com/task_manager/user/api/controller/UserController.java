b896a2f9f2e7b251dc987ccd88bfe59acfca09b7
Vítor Lougon <vitor.lougon@gmail.com>
2026-01-05 19:26:56 -0300

    feat: disabling unused controller

--- backend/task-manager/src/main/java/br/com/task_manager/user/api/controller/UserController.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/api/controller/UserController.java
@@ -1,15 +1,7 @@
 package br.com.task_manager.user.api.controller;
 
-import br.com.task_manager.user.api.dto.UserRequestDto;
-import br.com.task_manager.user.api.dto.UserResponseDto;
-import br.com.task_manager.user.api.mapper.UserMapper;
-import br.com.task_manager.user.application.dto.CreateUserCommand;
-import br.com.task_manager.user.application.dto.UserServiceResponseDto;
 import br.com.task_manager.user.application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,15 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
-
-    @PostMapping
-    public ResponseEntity<UserResponseDto> createUser (@RequestBody UserRequestDto request) {
-        CreateUserCommand userCommand = UserMapper.toCreateUserCommand(request);
-
-        UserServiceResponseDto serviceResponse = this.userService.createUser(userCommand);
-
-        UserResponseDto response = UserMapper.toResponseDto(serviceResponse);
-
-        return ResponseEntity.ok(response);
-    }
 }
