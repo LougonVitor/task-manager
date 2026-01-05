d76ef4f5ecb5f7188d6425766ccf572fe9516d7c
Vítor Lougon <vitor.lougon@gmail.com>
2026-01-05 14:32:43 -0300

    feat: creating token service

---
+++ backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/security/TokenService.java
@@ -0,0 +1,48 @@
+package br.com.task_manager.user.infrastructure.security;
+
+import br.com.task_manager.user.domain.entity.UserEntity;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(UserEntity user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("task-manager")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException ex) {
+            throw new RuntimeException("Error while generating token", ex);
+        }
+    }
+
+    public String ValideteToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("task-manager")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch(JWTVerificationException ex) {
+            return "";
+        }
+    }
+
+    private Instant genExpirationDate(){
+        // Sets the token to expire in 2 hours, adjusted to your timezone (e.g., -3 for Brazil)
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
