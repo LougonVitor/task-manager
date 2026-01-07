56a31c144a0c88865fb930b560826e38d9de21c3
Vítor Lougon <vitor.lougon@gmail.com>
2026-01-07 11:04:23 -0300

    fix: replacing the user entity with the user details

--- backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/security/TokenService.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/security/TokenService.java
@@ -1,11 +1,11 @@
 package br.com.task_manager.user.infrastructure.security;
 
-import br.com.task_manager.user.domain.entity.UserEntity;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -17,7 +17,7 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    public String generateToken(UserEntity user) {
+    public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
