c7b0289fdaff11cc5717242dc99886606a7fa355
Vítor Lougon <vitor.lougon@gmail.com>
2026-01-09 18:35:35 -0300

    feat: adding getters and setters to task entity

--- backend/task-manager/src/main/java/br/com/task_manager/task/domain/entity/TaskEntity.java
+++ backend/task-manager/src/main/java/br/com/task_manager/task/domain/entity/TaskEntity.java
@@ -5,10 +5,67 @@ import br.com.task_manager.task.domain.valueobject.TaskStatus;
 import java.time.LocalDateTime;
 
 public class TaskEntity {
+    private Long id;
     private String title;
     private String description;
     private TaskStatus taskStatus;
     private LocalDateTime createdAt;
     private LocalDateTime deadline;
     private LocalDateTime completedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(TaskStatus taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
 }
