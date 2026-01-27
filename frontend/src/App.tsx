e01abc8b67dacc714e100bba8a71739e540d7c14
Vítor Lougon <vitor.lougon@gmail.com>
2026-01-27 13:50:43 -0300

    feat: initializing home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Login } from './pages/login'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <>
+    <Login></Login>
     </>
   )
 }
---
+++ frontend/src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+export function Login() {
+    return (
+        <>
+            <section>
+                <div id="login-box">
+
+                </div>
+            </section>
+        </>
+    )
+}
