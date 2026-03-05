8947695e799627df8e36225b9a462acfd9ec1d22
Vítor Lougon <vitor.lougon@gmail.com>
2026-03-05 18:51:15 -0300

    cleanup (user): deleting unused imports

--- backend/task-manager/src/main/java/br/com/task_manager/user/api/dto/auth/CreateRequestDto.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/api/dto/auth/CreateRequestDto.java
@@ -1,5 +1,4 @@
 package br.com.task_manager.user.api.dto.auth;
 
-
 public record CreateRequestDto(String username, String email, String password, String role) {
 }
--- backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/repository/UserJpaRepository.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/repository/UserJpaRepository.java
@@ -1,9 +1,7 @@
 package br.com.task_manager.user.infrastructure.repository;
 
-import br.com.task_manager.user.domain.entity.UserEntity;
 import br.com.task_manager.user.infrastructure.entity.UserJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long> {
     UserJpaEntity findByUsername(String username);
