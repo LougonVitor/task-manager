6ad375507929e1771687d9b7266c63fe00e14e95
Vítor Lougon <vitor.lougon@gmail.com>
2026-03-05 18:48:25 -0300

    cleanup (user): deleting unused classes

--- backend/task-manager/src/main/java/br/com/task_manager/user/api/dto/auth/CreateRequestDto.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/api/dto/auth/CreateRequestDto.java
@@ -1,6 +1,5 @@
 package br.com.task_manager.user.api.dto.auth;
 
-import br.com.task_manager.user.domain.valueobject.UserRole;
 
 public record CreateRequestDto(String username, String email, String password, String role) {
 }
