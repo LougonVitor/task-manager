27f3ae8f2ab6dde708bc846e1d2145123bbbb533
Vítor Lougon <vitor.lougon@gmail.com>
2026-04-04 11:03:08 -0300

    raw: enabling cors

---
+++ backend/task-manager/src/main/java/br/com/task_manager/common/security/WebConfig.java
@@ -0,0 +1,23 @@
+package br.com.task_manager.common.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all paths
+                        .allowedOrigins("http://localhost:5173") // Allowed domain
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
