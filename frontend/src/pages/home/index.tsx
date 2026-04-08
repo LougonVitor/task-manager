0f19efa3a63755e75f5f8ca1e342b2d56c12cd58
Vítor Lougon <vitor.lougon@gmail.com>
2026-04-07 21:46:46 -0300

    feat: initializing home page with static components

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,8 +1,55 @@
+import './style.css';
+import { Plus } from 'lucide-react';
+import { TaskComponent } from '../../component/task';
+import { Header } from '../../component/header';
+
 export function Home() {
-    return (
-        <div className='home-page'>
-            <h1>Welcome to the Home Page!</h1>
-            <p>This is where you can manage your tasks and stay organized.</p>
+  const tasks = [
+    { id: 1, title: 'Integrate User Authentication', date: '22/11/2025', desc: 'Finalize the JWT token generation on the back-end...', completed: true },
+    { id: 2, title: 'Integrate User Authentication', date: '22/11/2025', desc: 'Finalize the JWT token generation on the back-end...', completed: false },
+    { id: 3, title: 'Buy Groceries for Dinner', date: '22/11/2025', desc: 'Milk, eggs, bread, and check the expiration date...', completed: false },
+  ];
+
+  return (
+    <>
+    <Header />
+
+    <div className="container">
+      {/* Header / Search Area */}
+      <div className="header-actions">
+        <div className="input-wrapper">
+          <input type="text" placeholder="..." />
+          <button className="add-btn"><Plus size={20} /></button>
         </div>
-    );
-}
+        <div className="filter-group">
+          <span>All</span>
+          <button className="filter-btn active">Completed</button>
+          <span>Pending</span>
+        </div>
+      </div>
+
+      {/* Completed Section */}
+      <section className="task-section">
+        <div className="section-header">
+          <h2>Completed</h2>
+          <span className="order-by">Order by: date</span>
+        </div>
+        {tasks.filter(t => t.completed).map(task => (
+          <TaskComponent id={task.id} title={task.title} date={task.date} description={task.desc} status={task.completed}/>
+        ))}
+      </section>
+
+      {/* To Do Section */}
+      <section className="task-section">
+        <div className="section-header">
+          <h2>To do</h2>
+          <span className="order-by">Order by: date</span>
+        </div>
+        {tasks.filter(t => !t.completed).map(task => (
+            <TaskComponent id={task.id} title={task.title} date={task.date} description={task.desc} status={task.completed}/>
+        ))}
+      </section>
+    </div>
+    </>
+  );
+};
