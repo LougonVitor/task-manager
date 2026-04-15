b3c4dde365701c2a9d459d499b5793c57a2bc4d9
Vítor Lougon <vitor.lougon@gmail.com>
2026-04-15 20:30:32 -0300

    feat: add useCreateTask hook

---
+++ frontend/src/hook/useCreateTask.ts
@@ -0,0 +1,34 @@
+import { useMutation } from '@tanstack/react-query';
+
+interface TaskRequest {
+    title: string;
+    description: string;
+    status: string;
+    deadline: string;
+}
+
+interface TaskResponse {
+    id: number;
+    title: string;
+    createdAt: string;
+}
+
+const API_URL = 'http://localhost:8080/task/create';
+
+export const useCreateTask = () => {
+    return useMutation<TaskResponse, Error, TaskRequest>({
+        mutationFn: async(task: TaskRequest) => {
+            const response = await fetch(API_URL, {
+                method: 'POST',
+                body: JSON.stringify(task)
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json().catch(() => ({}));
+                throw new Error(errorData.message || "Task creation failed!");
+            }
+
+            return response.json();
+        }
+    });
+}
