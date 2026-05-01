5ce9ebc564095707a5942b08ddef7e205c869c95
Vítor Lougon <vitor.lougon@gmail.com>
2026-05-01 16:05:43 -0300

    feat: validate if userId matches the userId of the task

--- frontend/src/component/task-modal/index.tsx
+++ frontend/src/component/task-modal/index.tsx
@@ -33,128 +33,127 @@ export function TaskModal({ task, onClose, isCreateModal, isDeleteModal, referen
     const { name, value } = e.target;
 
     setFormData((prev) => ({
-      ...prev,
-      [name]: value,
+        ...prev,
+        [name]: value,
     }));
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (isCreateModal) {
-      mutate(formData, {
-        onSuccess: (data) => {
-          console.log(data.createdAt.toString());
-          onClose();
-        },
-        onError: (error) => console.error(error.message)
-      });
-    }
+    if(!isCreateModal) {
+        console.log(task?.userId)
+        console.log(referenceId)
+        if (task?.userId != referenceId) return console.error("You cannot modify a task that is not yours!");
+        
+        if (isDeleteModal && task?.id) {
+            deleteTask(task.id, {
+                onSuccess: () => {onClose()},
+                onError: (error) => console.error(error.message)
+            });
+        } else if (task) {
+            const taskRequeste : Task = {
+                id: task.id,
+                title: formData.title || 'Untitled Task',
+                description: formData.description || '',
+                isCompleted: false,
+                deadline: formData.deadline ? new Date(formData.deadline) : new Date()
+            }
 
-    else if (isDeleteModal && task?.id) {
-      deleteTask(task.id, {
-        onSuccess: () => {onClose(); console.log("Modal closing");},
-        onError: (error) => console.error(error.message)
-      });
-    }
-
-    else if (task) {
-      console.log("Está chegando aqui")
-
-      const taskRequeste : Task = {
-        id: task.id,
-        title: formData.title || 'Untitled Task',
-        description: formData.description || '',
-        isCompleted: false,
-        deadline: formData.deadline ? new Date(formData.deadline) : new Date()
-      }
-
-      updateTask(taskRequeste, {
-        onSuccess: () => {onClose(); console.log("Sucesso")},
-        onError: (error) => console.error(error.message)
-      });
+            updateTask(taskRequeste, {
+                onSuccess: () => {onClose(); console.log("Success")},
+                onError: (error) => console.error(error.message)
+            });
+        }
+    } else {
+        mutate(formData, {
+            onSuccess: (data) => {
+                console.log(data.createdAt.toString());
+                onClose();
+            },
+            onError: (error) => console.error(error.message)
+        });
     }
   };
 
   return (
     <>
     <div className="modal-overlay">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h3 className="modal-title">
-            {isDeleteModal ? 'Confirm Deletion' : (isCreateModal ? 'Add New Task' : 'Edit Task')}
-          </h3>
+        <div className="modal-content">
+            <div className="modal-header">
+                <h3 className="modal-title">
+                    {isDeleteModal ? 'Confirm Deletion' : (isCreateModal ? 'Add New Task' : 'Edit Task')}
+                </h3>
+            </div>
+
+            <form className="task-form" onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label>Title</label>
+                    <input
+                        name='title'
+                        type="text"
+                        placeholder="Enter task title..."
+                        className="modal-input"
+                        {...isDeleteModal ? { disabled: true } : {}}
+                        value={formData.title}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                {isDeleteModal ? 
+                    <></> 
+                    : 
+                    <>
+                    <div className="input-group">
+                        <label>Deadline</label>
+                        <input
+                            name='deadline'
+                            type="date"
+                            className="modal-input"
+                            value= {formData.deadline ? new Date(formData.deadline).toISOString().slice(0, 10) : ''}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="input-group">
+                        <label>Description</label>
+                        <textarea
+                            name='description'
+                            placeholder="Description of the new task..."
+                            className="modal-input modal-textarea"
+                            value={formData.description}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                </>}
+
+                <div className="modal-footer">
+                    <button 
+                        type="button" 
+                        className="btn-cancel-link" 
+                        onClick={onClose}
+                    >
+                        Cancel
+                    </button>
+                    <button type="submit" className="btn-primary-teal">
+                        <Plus size={18} style={{ marginRight: '4px' }} />
+                        {isCreateModal && isPending
+                        ? 'Saving...'
+                        : isDeleteModal && isDeleting
+                        ? 'Deleting...'
+                        : !isCreateModal && !isDeleteModal && isEditing
+                        ? 'Editing...'
+                        : isDeleteModal
+                        ? 'Delete Task'
+                        : isCreateModal
+                        ? 'Add Task'
+                        : 'Edit Task'}
+                    </button>
+                </div>
+            </form>
         </div>
-
-        <form className="task-form" onSubmit={handleSubmit}>
-          <div className="input-group">
-            <label>Title</label>
-            <input
-              name='title'
-              type="text"
-              placeholder="Enter task title..."
-              className="modal-input"
-              {...isDeleteModal ? { disabled: true } : {}}
-              value={formData.title}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          {isDeleteModal ? 
-          <></> 
-          : 
-          <>
-          <div className="input-group">
-            <label>Deadline</label>
-            <input
-              name='deadline'
-              type="date"
-              className="modal-input"
-              value= {formData.deadline ? new Date(formData.deadline).toISOString().slice(0, 10) : ''}
-              onChange={handleChange}
-              required
-            />
-          </div>
-
-          <div className="input-group">
-            <label>Description</label>
-            <textarea
-              name='description'
-              placeholder="Description of the new task..."
-              className="modal-input modal-textarea"
-              value={formData.description}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          </>
-          }
-
-          <div className="modal-footer">
-            <button 
-              type="button" 
-              className="btn-cancel-link" 
-              onClick={onClose}
-            >
-              Cancel
-            </button>
-            <button type="submit" className="btn-primary-teal">
-              <Plus size={18} style={{ marginRight: '4px' }} />
-              {isCreateModal && isPending
-              ? 'Saving...'
-              : isDeleteModal && isDeleting
-              ? 'Deleting...'
-              : !isCreateModal && !isDeleteModal && isEditing
-              ? 'Editing...'
-              : isDeleteModal
-              ? 'Delete Task'
-              : isCreateModal
-              ? 'Add Task'
-              : 'Edit Task'}
-            </button>
-          </div>
-        </form>
-      </div>
     </div>
     </>
   );
