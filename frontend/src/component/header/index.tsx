d289e110335cbd7852960b1a44b9f4bbf35cc10a
Vítor Lougon <vitor.lougon@gmail.com>
2026-05-01 15:39:50 -0300

    feat: create logoff button

--- frontend/src/component/header/index.tsx
+++ frontend/src/component/header/index.tsx
@@ -1,19 +1,28 @@
+import { useNavigate } from 'react-router-dom';
 import './style.css';
 
 export function Header() {
-  return (
+    const navigate = useNavigate();
+
+    const handleLogoff = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    }
+
+    return (
     <>
     <header className="main-header">
-      <div className="header-container">
+        <div className="header-container">
         {/* Application Title */}
         <div className="app-title">My Tasks</div>
 
         {/* Navigation Links */}
         <nav className="header-nav">
-          <a href="#tasks" className="nav-link active">Tasks</a>
+            <a href="#tasks" className="nav-link active">Tasks</a>
+            <a className="nav-link logoff" onClick={(e) => {e.preventDefault(); handleLogoff();}}>Logoff</a>
         </nav>
-      </div>
+        </div>
     </header>
     </>
-  );
+    );
 };
