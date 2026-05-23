6f3d3164045ebca0d8c603f64f30c13f0d80af4e
Vítor Lougon <vitor.lougon@gmail.com>
2026-05-23 16:42:05 -0300

    feat: create global exception handler

---
+++ backend/task-manager/src/main/java/br/com/task_manager/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package br.com.task_manager.common.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String field = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(field, message);
+        });
+
+        return errors;
+    }
+}
