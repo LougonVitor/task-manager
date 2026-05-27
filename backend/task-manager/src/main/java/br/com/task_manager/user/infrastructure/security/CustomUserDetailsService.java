c7f3762a3087f6ed5dd5371b455b051d28630bf6
Vítor Lougon <vitor.lougon@gmail.com>
2026-05-26 21:59:10 -0300

    fix: handle missing user with UsernameNotFoundException in CustomUserDetailsService

--- backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/security/CustomUserDetailsService.java
+++ backend/task-manager/src/main/java/br/com/task_manager/user/infrastructure/security/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package br.com.task_manager.user.infrastructure.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,16 +8,20 @@ import org.springframework.stereotype.Service;
 import br.com.task_manager.user.domain.entity.UserEntity;
 import br.com.task_manager.user.domain.repository.IUserRepository;
 
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+    private final IUserRepository userRepository;
 
-    @Autowired
-    private IUserRepository userRepository;
+    public CustomUserDetailsService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity = this.userRepository.findByUsername(username);
+        Optional<UserEntity> userEntity = this.userRepository.findByUsername(username);
 
-        return new UserSecurityDetails(userEntity);
+        return new UserSecurityDetails(userEntity.orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username)));
     }
 }
